47eb7635149d0aceaad007315e7d445bbcfcf9f3
LeRoyce Pearson <contact@leroycepearson.dev>
2023-05-13 22:40:18 -0600

    Add mpt and replit examples to build.zig

--- build.zig
+++ build.zig
@@ -11,6 +11,7 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = optimize,
     });
     lib.want_lto = want_lto;
+    lib.c_std = .C11;
     lib.addIncludePath("include/ggml");
     lib.installHeadersDirectory("include/ggml", "ggml");
     lib.addCSourceFile("src/ggml.c", &.{});
@@ -18,14 +19,24 @@ pub fn build(b: *std.build.Builder) void {
     b.installArtifact(lib);
 
     // utils for examples
-    const example_utils_lib = b.addStaticLibrary(.{
-        .name = "ggml-example-utils",
+    const example_common_lib = b.addStaticLibrary(.{
+        .name = "common",
         .target = target,
         .optimize = optimize,
     });
-    example_utils_lib.addCSourceFile("examples/utils.cpp", &.{"-std=c++11"});
-    example_utils_lib.installHeader("examples/utils.h", "utils.h");
-    example_utils_lib.linkLibCpp();
+    example_common_lib.addCSourceFile("examples/common.cpp", &.{"-std=c++11"});
+    example_common_lib.installHeader("examples/common.h", "common.h");
+    example_common_lib.linkLibCpp();
+
+    const example_common_ggml_lib = b.addStaticLibrary(.{
+        .name = "common-ggml",
+        .target = target,
+        .optimize = optimize,
+    });
+    example_common_ggml_lib.linkLibrary(lib);
+    example_common_ggml_lib.addCSourceFile("examples/common-ggml.cpp", &.{"-std=c++11"});
+    example_common_ggml_lib.installHeader("examples/common-ggml.h", "common-ggml.h");
+    example_common_ggml_lib.linkLibCpp();
 
     // StableLM/GPTNeoX example
     const stablelm_example = b.addExecutable(.{
@@ -35,7 +46,8 @@ pub fn build(b: *std.build.Builder) void {
     });
     stablelm_example.addCSourceFile("examples/stablelm/main.cpp", &.{"-std=c++11"});
     stablelm_example.linkLibrary(lib);
-    stablelm_example.linkLibrary(example_utils_lib);
+    stablelm_example.linkLibrary(example_common_lib);
+    stablelm_example.linkLibrary(example_common_ggml_lib);
     b.installArtifact(stablelm_example);
 
     const stablelm_quantize = b.addExecutable(.{
@@ -45,6 +57,53 @@ pub fn build(b: *std.build.Builder) void {
     });
     stablelm_quantize.addCSourceFile("examples/stablelm/quantize.cpp", &.{"-std=c++11"});
     stablelm_quantize.linkLibrary(lib);
-    stablelm_quantize.linkLibrary(example_utils_lib);
+    stablelm_quantize.linkLibrary(example_common_lib);
+    stablelm_quantize.linkLibrary(example_common_ggml_lib);
     b.installArtifact(stablelm_quantize);
+
+    // Replit example
+    const replit_example = b.addExecutable(.{
+        .name = "replit",
+        .target = target,
+        .optimize = optimize,
+    });
+    replit_example.addCSourceFile("examples/replit/main.cpp", &.{"-std=c++11"});
+    replit_example.linkLibrary(lib);
+    replit_example.linkLibrary(example_common_lib);
+    replit_example.linkLibrary(example_common_ggml_lib);
+    b.installArtifact(replit_example);
+
+    const replit_quantize = b.addExecutable(.{
+        .name = "replit-quantize",
+        .target = target,
+        .optimize = optimize,
+    });
+    replit_quantize.addCSourceFile("examples/replit/quantize.cpp", &.{"-std=c++11"});
+    replit_quantize.linkLibrary(lib);
+    replit_quantize.linkLibrary(example_common_lib);
+    replit_quantize.linkLibrary(example_common_ggml_lib);
+    b.installArtifact(replit_quantize);
+
+    // MPT example
+    const mpt_example = b.addExecutable(.{
+        .name = "mpt",
+        .target = target,
+        .optimize = optimize,
+    });
+    mpt_example.addCSourceFile("examples/mpt/main.cpp", &.{"-std=c++11"});
+    mpt_example.linkLibrary(lib);
+    mpt_example.linkLibrary(example_common_lib);
+    mpt_example.linkLibrary(example_common_ggml_lib);
+    b.installArtifact(mpt_example);
+
+    const mpt_quantize = b.addExecutable(.{
+        .name = "mpt-quantize",
+        .target = target,
+        .optimize = optimize,
+    });
+    mpt_quantize.addCSourceFile("examples/mpt/quantize.cpp", &.{"-std=c++11"});
+    mpt_quantize.linkLibrary(lib);
+    mpt_quantize.linkLibrary(example_common_lib);
+    mpt_quantize.linkLibrary(example_common_ggml_lib);
+    b.installArtifact(mpt_quantize);
 }
